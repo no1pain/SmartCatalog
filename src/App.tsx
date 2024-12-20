535c3c1fea7338fe00f77c18aef4ef706d924192
Oleksandr Kazan <kazansasha04@gmail.com>
2024-12-20 23:40:38 +0200

    header added

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.scss';
+import './index.css';
+import { Header } from './components/Header/Header';
+
+export const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div className="header h-12 flex items-center justify-between pl-4 pr-4 border-b border-[#3B3E4A]">
+      <div className="header__logo bg-contain bg-no-repeat bg-center w-16 h-5"></div>
+      <div className="header__menu"></div>
+      <div className="header__right"></div>
+      <div className="header__menu w-4 h-4 bg-contain bg-center"></div>
+    </div>
+  );
+};
